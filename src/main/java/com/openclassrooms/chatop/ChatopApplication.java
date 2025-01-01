b1a7b81cd3c610c6c352d9e2f30e3f64deee6198
ay63 <74818855+ay63@users.noreply.github.com>
2025-01-01 17:19:09 +0100

    feat: add jwt token authentication

--- src/main/java/com/openclassrooms/chatop/ChatopApplication.java
+++ src/main/java/com/openclassrooms/chatop/ChatopApplication.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.chatop.chatop;
+package com.openclassrooms.chatop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
