38193f8f9fb0f8dc784c2a4e1f7ad4b63114a81c
ay63 <74818855+ay63@users.noreply.github.com>
2025-01-11 16:29:54 +0100

    doc: improvement swagger

--- src/main/java/com/openclassrooms/chatop/controllers/UserController.java
+++ src/main/java/com/openclassrooms/chatop/controllers/UserController.java
@@ -2,8 +2,12 @@ package com.openclassrooms.chatop.controllers;
 
 import com.openclassrooms.chatop.dto.user.UserResponseDto;
 import com.openclassrooms.chatop.entities.User;
-import com.openclassrooms.chatop.exceptions.NotFoundException;
 import com.openclassrooms.chatop.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +24,28 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            description = "Get user by id",
+            tags = {"User"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "return user data",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "unauthorized"
+            )
+    })
     @GetMapping(path = "{id}")
     public ResponseEntity<UserResponseDto> getUser(@PathVariable("id") int id) {
         User user = this.userService.findUserByIdOrThrowError(id);
